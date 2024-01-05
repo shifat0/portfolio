a756becb046df15f3baff95d5deb6a90e0c23492
Md. Shoaib Akhter Shifat <sifuisu3@gmail.com>
2024-01-05 23:28:06 +0600

    basic nav desinged

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,3 +1,14 @@
 export const Nav = () => {
-  return <nav>List of Navs to Id</nav>;
+  return (
+    <nav className="bg-slate-500 text-white p-5">
+      <div className="container flex justify-between align-middle">
+        <span>Shifat</span>
+        <ul className="flex align-middle gap-8">
+          <li>About</li>
+          <li>Projects</li>
+          <li>Contact Me</li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
